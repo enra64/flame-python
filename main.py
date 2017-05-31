7b5140fa8e51ddebeb7dc0dc6744f88f58d3f11a
enra64 <sujomear@arcor.de>
2017-05-31 12:43:26 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-import json
-import urllib
 import test
 
 from structure_information_extraction import extract_structure_information
 from fuzzy_approximation import fuzzy_approximation
-#from cluster_construction import cluster_construction
+
+
+# from cluster_construction import cluster_construction
 
 def flame_cluster(data, k, outlier_threshold, distance_measure):
     """
@@ -42,30 +42,9 @@ def flame_cluster(data, k, outlier_threshold, distance_measure):
     return fuzzy_approximation(data, k, 10, *structure_information)
 
 
-def dl_sets():
-    """Helper function to try and download all data sets for testing purposes"""
-    with open('datasets.json') as data_file:
-        data = json.load(data_file)
-
-    no = 1
-    for set in data.itervalues():
-        name = set["name"]
-        training = "http://www.learning-challenge.de" + set["url_training_arff"].rstrip("/")
-        test = "http://www.learning-challenge.de" + set["url_test_arff"].rstrip("/")
-
-        if "data" in test:
-            print(test)
-        if "data" in training:
-            print(training)
-        urllib.urlretrieve(test, "datasets/" + name + '_test.arff')
-        urllib.urlretrieve(training, "datasets/" + name + '_training.arff')
-
-        no += 1
-
-
 if __name__ == "__main__":
     """
     If run as main, all tests will be run
     """
     test.test_iris_euclidean(lambda data, measure: flame_cluster(data, 10, 0.1, measure))
-    #test.run_tests(lambda data, measure: flame_cluster(data, 3, 0.1, measure))
+    # test.run_tests(lambda data, measure: flame_cluster(data, 3, 0.1, measure))
